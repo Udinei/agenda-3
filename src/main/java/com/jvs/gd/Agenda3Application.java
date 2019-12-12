9478cb5d342a816037b7db4981b6a4de1f891b98
Udinei Silva <ukaliko@gmail.com>
2019-12-12 11:51:34 -0400

    Projeto criado

---
+++ src/main/java/com/jvs/gd/Agenda3Application.java
@@ -0,0 +1,13 @@
+package com.jvs.gd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Agenda3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Agenda3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jvs/gd/Agenda3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvs.gd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Agenda3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
